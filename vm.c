491f27b5f7a9331cc41cb5998d17aa9a435a97f7
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2023-12-29 16:08:11 -0600

    15.2.2: add stack tracing to the run methods

--- vm.c
+++ vm.c
@@ -16,6 +16,16 @@ void initVM(){
 void freeVM(){
 }
 
+void push(Value value) {
+	*vm.stackTop = value;
+	vm.stackTop++;
+}
+
+Value pop() {
+	vm.stackTop--;
+	return *vm.stackTop;
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -23,6 +33,13 @@ static InterpretResult run() {
 	for(;;) {
 		
 #ifndef DEBUG_TRACE_EXECUTION
+	printf("          ");
+	for(Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+		printf("[ ");
+		printValue(*slot);
+		printf(" ]");
+	}
+	printf("\n")
 	disassembleInstruction(vm.chunk,
 							(int)(vm.ip - vm.chunk->code));
 #endif
@@ -31,11 +48,12 @@ static InterpretResult run() {
 		switch(instruction = READ_BYTE()) {
 			case OP_CONSTANT: {
 				Value constant = READ_CONSTANT();
-				printValue(constant);
-				printf("\n");
+				push(constant);
 				break;
 			}
 			case OP_RETURN: {
+				printValue(pop());
+				printf("\n");
 				return INTERPRET_OK;
 			}
 		}
